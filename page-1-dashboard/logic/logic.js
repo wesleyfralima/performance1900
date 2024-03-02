a417ba0da641ca6be99cd9fc74b689734cd1379e
wesleyfralima <wesleyfralima@gmail.com>
2024-03-02 20:21:23 -0300

    Minor improvements.

--- page-1-dashboard/logic/logic.js
+++ page-1-dashboard/logic/logic.js
@@ -3,13 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     var root = document.querySelector(':root');
     var rootStyles = getComputedStyle(root);
 
-    var color = rootStyles.getPropertyValue('--color');
+    var color = null;
 
-    var porcentagem1 = document.querySelector('#porcentagem-grafico-1').innerHTML;
+    var porcentagem1 = parseFloat(document.querySelector('#porcentagem-grafico-1').innerHTML.replace('%', '').replace(',', '.'));
+    var porcentagem2 = parseFloat(document.querySelector('#porcentagem-grafico-2').innerHTML.replace('%', '').replace(',', '.'));
+    var porcentagem3 = parseFloat(document.querySelector('#porcentagem-grafico-3').innerHTML.replace('%', '').replace(',', '.'));
 
-    if (parseFloat(porcentagem1.replace('%', '')) >= 0) {
-        test = true
-    } else {
-        test = false
-    }
+    
+
 });
