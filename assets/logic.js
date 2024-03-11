86636c495e6f7d254469054c5e7eddf9c80b5c7f
wesleyfralima <wesleyfralima@gmail.com>
2024-03-10 22:16:41 -0300

    Several changes.

--- assets/logic.js
+++ assets/logic.js
@@ -18,3 +18,18 @@ function verProtocolo(target) {
     alert(protocolo.innerHTML)
     window.open("https://www.google.com.br/?q=" + protocolo.innerHTML, "_blank");
 }
+
+filtrar = () => {
+    const trs = document.querySelectorAll('#avisos tr:not(.header)')
+    const filter = document.querySelector('#barra-pesquisa').value
+    const regex = new RegExp(filter, 'i')
+    const isFoundInTds = td => regex.test(td.innerHTML)
+    const isFound = childrenArr => childrenArr.some(isFoundInTds)
+    const setTrStyleDisplay = ({ style, children }) => {
+        style.display = isFound([
+            ...children // <-- All columns
+        ]) ? '' : 'none' 
+    }
+
+    trs.forEach(setTrStyleDisplay)
+};
