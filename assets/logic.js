c358a8e797f0338294d6b82e251bf1eeba64c6eb
wesleyfralima <wesleyfralima@gmail.com>
2024-03-26 00:22:07 -0300

    Inserted pages url and cleaned logic.js file.

--- assets/logic.js
+++ assets/logic.js
@@ -1,24 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    var root = document.querySelector(':root');
-    var rootStyles = getComputedStyle(root);
-
-    var color = null;
-
-    var porcentagem1 = parseFloat(document.querySelector('#porcentagem-grafico-1').innerHTML.replace('%', '').replace(',', '.'));
-    var porcentagem2 = parseFloat(document.querySelector('#porcentagem-grafico-2').innerHTML.replace('%', '').replace(',', '.'));
-    var porcentagem3 = parseFloat(document.querySelector('#porcentagem-grafico-3').innerHTML.replace('%', '').replace(',', '.'));
-
-    
-
-});
-
-function verProtocolo(target) {
-    protocolo = target.firstChild;
-    alert(protocolo.innerHTML)
-    window.open("https://www.google.com.br/?q=" + protocolo.innerHTML, "_blank");
-}
-
 filtrar = () => {
     const trs = document.querySelectorAll('#avisos tr:not(.header)')
     const filter = document.querySelector('#barra-pesquisa').value
